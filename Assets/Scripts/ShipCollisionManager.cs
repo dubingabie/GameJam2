9ec60d92eb72a69348df0bcbe8064a40b79a41d4
dubingabie <gaberiel.dubin@mail.huji.ac.il>
2024-12-08 12:17:17 +0200

    ship collision fixed

---
+++ Assets/Scripts/ShipCollisionManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCollisionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("GolfBall"))
+        {
+            Debug.Log("golf ball hit the ship");
+            //Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
