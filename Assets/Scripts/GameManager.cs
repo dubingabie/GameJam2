7d28f6e7da02e5c256a088f0edb32697741fbbe9
dubingabie <gaberiel.dubin@mail.huji.ac.il>
2024-12-11 14:34:23 +0200

    game over screen now triggers when all of the ships are destroyed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,14 @@ public class GameManager : MonoBehaviour
     public Transform spaceshipsParent;
     public int rowCount = 2;
     public int shipsPerRow = 5;
+    [SerializeField] GameOverManager gameOverManager;  // Reference to your GameOverManager
+    
+    private int remainingShips;  // Track remaining ships
+
     void Start()
     {
         SpawnShipFormation();
+        remainingShips = rowCount * shipsPerRow;
     }
 
     void SpawnShipFormation()
@@ -25,6 +30,27 @@ public class GameManager : MonoBehaviour
                 Debug.Log(spawnPosition);
                 GameObject spawnedShip = Instantiate(shipPrefab, spawnPosition, Quaternion.identity);
                 spawnedShip.transform.SetParent(spaceshipsParent);
+                
+                // Add a reference to the GameManager in collision manager of each shipp
+                ShipCollisionManager collisionManager = spawnedShip.GetComponent<ShipCollisionManager>();
+                if (collisionManager != null)
+                {
+                    collisionManager.gameManager = this;
+                }
+                
+            }
+        }
+    }
+    public void OnShipDestroyed()
+    {
+        remainingShips--;
+        Debug.Log($"Ships remaining: {remainingShips}");
+        
+        if (remainingShips <= 0)
+        {
+            if (gameOverManager != null)
+            {
+                gameOverManager.ShowGameOver();
             }
         }
     }
--- Assets/Scripts/ShipCollisionManager.cs
+++ Assets/Scripts/ShipCollisionManager.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipCollisionManager : MonoBehaviour
-{
+{   
+     public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -22,6 +22,8 @@ public class ShipCollisionManager : MonoBehaviour
         {
             Debug.Log("golf ball hit the ship");
             //Destroy(other.gameObject);
+            // trigger on ship destroy method of the game manager
+            gameManager.OnShipDestroyed();
             Destroy(gameObject);
         }
     }
