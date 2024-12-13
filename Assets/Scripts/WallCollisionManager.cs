6e97374120f4907e7d43b75db9efd4a7d01f9979
dubingabie <gaberiel.dubin@mail.huji.ac.il>
2024-12-13 19:04:40 +0200

    added wall destruction animation

--- Assets/Scripts/WallCollisionManager.cs
+++ Assets/Scripts/WallCollisionManager.cs
@@ -7,9 +7,13 @@ using UnityEngine;
 public class WallCollisionManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Sprite[] destructionSprites;
+    [SerializeField] float frameRate = 0.1f;
+    private SpriteRenderer spriteRenderer;
+    private bool isDestroying = false;
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,9 +30,26 @@ public class WallCollisionManager : MonoBehaviour
             //Destroy(other.gameObject);
             // maybe add multiple hits to wall before it gets destroyed
             // make the other object dissapear 
+            isDestroying = true;
+
             other.gameObject.SetActive(false);
-            Destroy(gameObject);
-            
+            //Destroy(gameObject);
+            StartCoroutine(PlayDestructionAnimation());
+
         }
+    }
+    private IEnumerator PlayDestructionAnimation()
+    {
+        // Disable collider to prevent multiple hits
+        GetComponent<Collider2D>().enabled = false;
+        // Play through each destruction frame
+        for (int i = 0; i < destructionSprites.Length; i++)
+        {
+            spriteRenderer.sprite = destructionSprites[i];
+            yield return new WaitForSeconds(frameRate);
+        }
+
+        // Notify game manager and destroy ship
+        Destroy(gameObject);
     }
 }
