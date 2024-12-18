6d96ef21c126d986630dc7825c32115122264bb2
dubingabie <gaberiel.dubin@mail.huji.ac.il>
2024-12-18 16:36:46 +0200

    added loading screen in the beginning

--- Assets/Scripts/VideoSceneTransition.cs
+++ Assets/Scripts/VideoSceneTransition.cs
@@ -1,14 +1,16 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class VideoSceneTransition : MonoBehaviour
 {
     [SerializeField] string nextSceneName; // Name of the scene to load
     [SerializeField] VideoPlayer videoPlayer;
-
-    void Start()
+    [SerializeField] TextMeshProUGUI loadngText;
+    void Awake()
     {
         // If not assigned in inspector, try to get the VideoPlayer component
         if (videoPlayer == null)
@@ -19,6 +21,8 @@ public class VideoSceneTransition : MonoBehaviour
         // Check if VideoPlayer is found
         if (videoPlayer != null)
         {
+            videoPlayer.started += OnVideoStarted;
+            // disable the loading text
             // Subscribe to the event that triggers when the video finishes
             videoPlayer.loopPointReached += OnVideoFinished;
         }
@@ -34,7 +38,10 @@ public class VideoSceneTransition : MonoBehaviour
         // Load the next scene
         SceneManager.LoadScene(nextSceneName);
     }
-
+    void OnVideoStarted(VideoPlayer source)
+    {
+        loadngText.gameObject.SetActive(false);
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
